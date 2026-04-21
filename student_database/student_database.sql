b3d2a039e80941bcefbf2816d84c18ba6b4b2643
Ryan <ryantonypereira@gmail.com>
2026-04-21 14:30:36 -0500

    Documentation
    Adding documentation to whole entire program. Line by line, adding documentation to explain purpose of the line and how it contributes to whole program.

--- student_database/student_database.sql
+++ student_database/student_database.sql
@@ -1,27 +1,46 @@
+-- Name: Ryan Pereira
+-- Project Name: Dice Roll
+-- Description: (Note: This file contains SQL database setup for student information, not a dice program.)
+-- Module Name: studentInformation.sql
+-- Module Purpose: Create and modify a student database table with basic student attributes.
+-- Collaborators: None
+-- Sources: Github Copilot, Stack Overflow, ChatGPT, Python documentation
+-- Date: 4/21/2026
+-- Last Modified: 4/21/2026
 
 
-
+-- Creating a database named studentInformation
 CREATE DATABASE studentInformation;
 
+-- Using the database
 USE studentInformation;
 
 
-
+-- Creating a table named student
 CREATE TABLE student(
+    -- Student ID column
     id INT,
-    firstName VARCHAR(20),
-    lastName VARCHAR(20),
-    age INT,
-    grade INT
 
+    -- First name column
+    firstName VARCHAR(20),
+
+    -- Last name column
+    lastName VARCHAR(20),
+
+    -- Age column
+    age INT,
+
+    -- Grade column
+    grade INT
 );
 
 
+-- Altering the student table to add a gender column
 ALTER TABLE student
 ADD gender VARCHAR(10);
 
+-- Selecting all columns from the student table
 SELECT * FROM student;
-
 
 
 
